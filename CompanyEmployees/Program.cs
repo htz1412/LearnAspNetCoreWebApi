b94cef7d4a6278c0e3c2200caba23e233c24d0f8
Harsh Gohel <harsh22gohel@gmail.com>
2023-09-27 20:08:01 +0530

    Completed "3.7 Creating a Repository Manager" section.

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -9,6 +9,7 @@ LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "\\n
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
 
 builder.Services.AddControllers();
 
